447b20de95d43c273e676fa838e5b2ad4f128890
anna-moore <ammoore434@gmail.com>
2020-11-17 16:07:04 -0600

    comments on FishList.js

--- scripts/FishList.js
+++ scripts/FishList.js
@@ -4,10 +4,11 @@ import { Fish } from './Fish.js'
 
 export const FishList = () => {
 
-    // Get a reference to the `<article class="content">` element
+    // Get a reference to the <article class= "fishList"> element
     const contentElement = document.querySelector(".fishList")
+    //grabs copy on the fish list
     const allTheFish = useFish()
-
+    //iterates through fish collect, translates objects to HTML, places HTML on DOM
     for (const fishObject of allTheFish){
        const fishHTML = Fish(fishObject)
        contentElement.innerHTML += fishHTML

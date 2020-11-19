ebf37302e15586ce058544e0dd4be387b7496e04
anna-moore <ammoore434@gmail.com>
2020-11-19 11:04:50 -0600

    filter by multples of 3,5, and none

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,16 @@ import { FishList } from "./fish/FishList.js"
 import { TipList } from "./tips/TipList.js"
 import { LocationList } from "./locations/LocationList.js"
 import { QuoteList } from "./quotes/QuoteList.js"
+import { mostHolyFish } from "./fish/FishDataProvider.js"
+import { soldierFish } from "./fish/FishDataProvider.js"
+import { nonHolyFish } from "./fish/FishDataProvider.js"
 
 FishList()
 TipList()
 LocationList()
 QuoteList()
 
+//test of fish sorting
+console.log("multiple of three",mostHolyFish(), "multiple of 5",soldierFish(), "regular fish",nonHolyFish())
  
 

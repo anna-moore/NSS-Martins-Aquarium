93f5058a1e6fd7d632f67e84031f8c2346316c13
anna-moore <ammoore434@gmail.com>
2020-12-02 09:38:19 -0600

    update to map() for QuoteList.js

--- scripts/quotes/QuoteList.js
+++ scripts/quotes/QuoteList.js
@@ -7,8 +7,14 @@ export const QuoteList = () =>{
     //Grab a copy on the quote list
     const allTheQuotes = useQuote()
     //iterates through quote collection, translates objects to HTML, place HTML on DOM
-    for(const quoteObject of allTheQuotes){
-        const quoteHTML = Quote(quoteObject)
-        contentElement.innerHTML += quoteHTML
-    }
+    // for(const quoteObject of allTheQuotes){
+    //     const quoteHTML = Quote(quoteObject)
+    //     contentElement.innerHTML += quoteHTML
+    // }
+    //replace for ..of with map()
+    contentElement.innerHTML+= `
+        <article class="quotes">
+            ${allTheQuotes.map(quoteObject => Quote(quoteObject)).join("")}
+        </article>
+        `
 } 

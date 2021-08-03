92e4cddf5d98506a33a373eaeee8f4f0064db495
anna-moore <ammoore434@gmail.com>
2021-08-03 16:53:07 -0500

    update margins and padding

--- scripts/fish/Fish.js
+++ scripts/fish/Fish.js
@@ -1,7 +1,7 @@
 /** Fish which renders individual fish objects as HTML (translator)*/
 export const Fish = (fish) => {
     return `
-        <section class="fish card">
+        <section class="fish_card">
             <div><img  class="fish__image image--card" src="media/${fish.image}" /></div>
             <div class="fish__name">${fish.name}</div>
             <div class="fish__species">${fish.species}</div>
